c7c6e65fbd69699cd2c782d77acc964260826840
Abhishek Dobhal <Abhishek.dobhal3@gmail.com>
2025-11-02 21:26:08 +0530

    Routing , made a footer

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 - Remove unncessary code 
 - Installed tailwind css (read documentation of tailwind for upgdrade with vite V3)
 - Installed Daisy UI and added Navbar component to App.jsx
+- Setup BroswerRouter or ReactRouter and installed react router dom
+- Initialised basic routes 
+- Create an Outlet in your body component for children routes
+- Create a Footer
+- Create a Login Page
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import NavBar from "./components/NavBar"
+// import NavBar from "./components/NavBar"
+import { BrowserRouter,Route,Routes } from "react-router"
+import Body from "./Body"
+import Profile from "./components/Profile"
+import Login from "./components/Login"
 
 function App() {
 
   return (
     <>
-    <NavBar></NavBar>
-     <p className="text-3xl">Hello world </p>
-     
+    {/* <NavBar></NavBar> */}
+<BrowserRouter>
+    <Routes>
+         <Route path="/" element={<Body/>} >
+         <Route path="/login" element={<Login/>} />
+         <Route path="/profile" element={<Profile/>}/>
+         </Route>
+  
+    </Routes>
+</BrowserRouter>     
     </>
   )
 }
